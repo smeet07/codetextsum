e2a3f416ead739fd583b7f3dc73204497f4ba9ac
smeet07 <smeetbn@gmail.com>
2022-11-06 21:49:07 +0530

    hide django key

--- summariser/summariser/settings.py
+++ summariser/summariser/settings.py
@@ -20,8 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=5@s)@fd5b92mf3n%+8lsxvr6w_bs2r*a5=t)az7cexrq)(4b7'
 
+from decouple import config
+
+SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
