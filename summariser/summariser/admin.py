4b88350ac71efd56d0cf3a04de214bbc40e80e4c
smeet07 <smeetbn@gmail.com>
2022-11-11 21:07:35 +0530

    trying to add img files

--- summariser/summariser/admin.py
+++ summariser/summariser/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import TextSum,PythonSum
+from .models import TextSum,PythonSum,TextImgSum
 class TextSumAdmin(admin.ModelAdmin):
       TextSum    = ['text', 'data', 'summary']
 class PySumAdmin(admin.ModelAdmin):
     list1=['code','summary']
 admin.site.register(PythonSum,PySumAdmin)
 admin.site.register(TextSum, TextSumAdmin)
+admin.site.register(TextImgSum)
--- summariser/summariser/models.py
+++ summariser/summariser/models.py
@@ -6,3 +6,7 @@ class TextSum(models.Model):
 class PythonSum(models.Model):
     code=models.TextField(blank=True,null=True)
     summary=models.CharField(max_length=500,blank=True,null=True)
+class TextImgSum(models.Model):
+    name=models.CharField(max_length=200, blank=True,null=True)
+    img=models.ImageField(upload_to='summariser/files/imgs')
+    # summary=models.CharField(max_length=1000,blank=True,null=True)
